0956ba2e256e17befde4ca8710b2330234f6d311
kssomabes <kssomabes@up.edu.ph>
2017-07-16 15:37:44 +0800

    uuid generator included in pets_controller; updates (PUT method) for pet info is now supported

--- entities/pets/pets_controller.js
+++ entities/pets/pets_controller.js
@@ -1,16 +1,13 @@
 const connection = require('./../../database/connection');
+const uuid = require('shortid');
+uuid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');	
+// default has "-"" and "_" ; this sets the characters to only the entered characters (https://www.npmjs.com/package/shortid)  
 
-// queries for pets are all here
+/* queries for pets are all here */
 
+/* view all pets */
 module.exports.viewShelterPets = function(callback){
 	connection.query('SELECT * FROM pets_of_shelters', function(err, results){
-		if (err) return calbacK(err); 	// some error with query
-		return callback(null, results); // if successful
-	});
-}
-
-module.exports.viewShelterPetsOf = function(shelter_Username, callback){
-	connection.query('SELECT * FROM pets_of_shelters WHERE shelter_Username = ?', shelter_Username, function(err, results){
 		if (err) return calbacK(err); 	// some error with query
 		return callback(null, results); // if successful
 	});
@@ -23,6 +20,14 @@ module.exports.viewUserPets = function(callback){
 	});
 }
 
+/* view pet of specific user/shelter */
+module.exports.viewShelterPetsOf = function(shelter_Username, callback){
+	connection.query('SELECT * FROM pets_of_shelters WHERE shelter_Username = ?', shelter_Username, function(err, results){
+		if (err) return calbacK(err); 	// some error with query
+		return callback(null, results); // if successful
+	});
+}
+
 module.exports.viewUserPetsOf = function(user_Username, callback){
 	connection.query('SELECT * FROM pets_of_users WHERE user_Username = ?', user_Username, function(err, results){
 		if (err) return calbacK(err); 	// some error with query
@@ -30,7 +35,10 @@ module.exports.viewUserPetsOf = function(user_Username, callback){
 	});
 }
 
+/* adding a pet */
 module.exports.addShelterPet = function(petInfo, callback){
+	petInfo.uuid = uuid.generate();
+	console.log(petInfo);
 	connection.query('INSERT INTO pets_of_shelters SET ?', petInfo, function(err, results){
 		if (err) return callback(err);	// some error with query
 		return callback(null, results);	// if successful
@@ -38,8 +46,25 @@ module.exports.addShelterPet = function(petInfo, callback){
 }
 
 module.exports.addUserPet = function(petInfo, callback){
+	petInfo.uuid = uuid.generate();
+	console.log(petInfo);
 	connection.query('INSERT INTO pets_of_users SET ?', petInfo, function(err, results){
 		if (err) return callback(err);	// some error with query
 		return callback(null, results);	// if successful
+	});
+}
+
+/* updating pet infos */
+module.exports.updateUserPet = function(uuid, changes, callback){
+	connection.query('UPDATE pets_of_users SET ? WHERE uuid = ?', [changes, uuid], function(err, results){
+		if (err) return callback(err);	// some error with query
+		return callback(null, changes);	// if successful
+	});
+}
+
+module.exports.updateShelterPet = function(uuid, changes, callback){
+	connection.query('INSERT INTO pets_of_shelters SET ? WHERE uuid = ?', [changes, uuid], function(err, results){
+		if (err) return callback(err);	// some error with query
+		return callback(null, changes);	// if successful
 	});
 }
--- entities/pets/pets_routes.js
+++ entities/pets/pets_routes.js
@@ -38,7 +38,7 @@ router.get('/:owner/viewUserPets', function(req, res){
     }); 
 });
 
-/* add pets to database */
+/* add pets to database (only if logged in) */
 router.post('/addShelterPet', function(req, res){
     console.log(req.session.body);
     if (req.session.body){
@@ -58,7 +58,7 @@ router.post('/addShelterPet', function(req, res){
             if (err) return res.status(500).json(err);  // server error
             res.status(201).json(pets); // returns pets of specified user
         });
-    }else res.status(401).json({message: 'login first before adding'});
+    }else res.status(403).json({message: 'login first before adding'});
 });
 
 router.post('/addUserPet', function(req, res){
@@ -79,7 +79,30 @@ router.post('/addUserPet', function(req, res){
             if (err) return res.status(500).json(err);  // server error
             res.status(201).json(results); // returns pets of specified user
         });
-    }else res.status(401).json({message: 'login first before adding'});
+    }else res.status(403).json({message: 'login first before adding'});
+});
+
+/* update certain info of pets (only if logged in && pet exists) */
+router.put('/:pet_uuid/updateShelterPets', function(req, res){
+    if (req.session.body){
+        var pet_uuid = req.params.pet_uuid; 
+        var changes = req.body;
+        controller.viewUserPetsOf(pet_uuid, changes, function(err, pets){
+            if (err) return res.status(500).json(err);  // server error
+            res.json(pets); // returns pets of specified user
+        }); 
+    }else res.status(403).json({message: 'login first before updating'});
+});
+
+router.put('/:pet_uuid/updateUserPets', function(req, res){
+    if(req.session.body){
+        var pet_uuid = req.params.pet_uuid;
+        var changes = req.body;
+        controller.viewUserPetsOf(pet_uuid, changes, function(err, pets){
+            if (err) return res.status(500).json(err);  // server error
+            res.json(pets); // returns pets of specified user
+        }); 
+    }else res.status(403).json({message: 'login first before updating'});
 });
 
 module.exports = router;
