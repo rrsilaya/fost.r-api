1431190fca873369ed085feb0cf7b0e97f9bc40e
kssomabes <kssomabes@up.edu.ph>
2017-07-16 10:21:01 +0800

    added and removed some dependencies

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-
 //require packages needed
-
 //https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
 
 const express =require('express');
 const app=express();
 const bodyParser =require('body-parser');
 const mysql=require('mysql');
+const logger = require('morgan');
 const connection=require('./database/connection');
 const routes=require('./routes.js');
-
 //configure app to use bodyParser()
-//
+
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended:true
 }))
-
 app.use('/api', routes);
 
 
 //https://hackernoon.com/restful-api-design-with-node-js-26ccf66eab09
 const port = process.env.PORT || 3000;
-//stat the server
+
+//start the server
 const server =app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
