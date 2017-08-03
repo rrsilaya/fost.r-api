e231db46773775850f3fcd8c0a845fc7333bc345
esevangelista <esevangelista1@up.edu.ph>
2017-08-03 18:53:46 +0800

    changes

--- entities/signup/signup_routes.js
+++ entities/signup/signup_routes.js
@@ -149,7 +149,7 @@ router.post('/shelter', function(req, res, next) {
           case 'TAKEN_UN_USERS':
             errors ='Username is already used by a shelter';
             console.log(errors);
-            return res.status(400).json(err);
+            return res.status(400).json(callback);
             break;
         }
       });
@@ -280,7 +280,7 @@ router.post('/user', function(req, res, next) {
           case 'TAKEN_UN_SHELTERS':
             errors ='Username is already used by a user';
             console.log(errors);
-            return res.status(400).json(err);
+            return res.status(400).json(errors);
             break;
         }
       });
